d00f157e29f4d0d184ba2e65d8316832a4715a5a
sean van wyk <svw.sysdev@gmail.com>
2020-10-16 03:43:38 +0200

    version 2.1 documentation

--- src/Home.ino
+++ src/Home.ino
@@ -6,13 +6,14 @@
 #include "Connection.h"
 #include "PinLogic.h"
 
-
-
 void setup() {
+//Call connector function located in Connection.h
 connector();
+//Call pinSetup function located in PinLogic.h
 pinSetup();
 }
 
 void loop(){
+//Call checkConnect function located in Connection.h
 checkConnect();
 }
--- src/PinLogic.h
+++ src/PinLogic.h
@@ -1,14 +1,17 @@
 void pinSetup() {
+    //Variables set to instantiate pin values 
     int pin = 0;
-    int spin = digitalRead(pin);
-    int butval = 0;
-
+    //Delay set to allow the previous function in Home.ino to finish.
     delay(1500);
+    //Open serial listner on 9600 baud rate.
     Serial.begin(9600);
-
+    //For loop declared to loop through the pin values to be set.
     for (int pin = 22; pin < 43; pin++) {
+        //Set the pin values to output.
         pinMode(pin, OUTPUT);
+        //Check state of each pin initially.
         digitalRead(pin);
+        //Set each pin to preffered state. 
         digitalWrite(pin, HIGH);
     }
 }
